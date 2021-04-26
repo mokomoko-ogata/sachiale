6109fd01f55e0a3fc1c46bdaa52ab749d3e4fe23
mokomoko-ogata <nobodyknows2405@gmail.com>
2021-04-26 22:18:11 +0900

    バリデーション設定

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,11 @@
 class Blog < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :recipe_name
+    validates :explain
+    validates :price
+    validates :image
+  end
 end

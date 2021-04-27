d47555e129c99a60a7a1d25197b090233060bc27
mokomoko-ogata <nobodyknows2405@gmail.com>
2021-04-27 11:55:30 +0900

    バリデーションの設定

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,9 +3,9 @@ class Blog < ApplicationRecord
   has_one_attached :image
 
   with_options presence: true do
-    validates :recipe_name
-    validates :explain
-    validates :price
+    validates :recipe_name, length: { maximum: 40 }
+    validates :explain, length: { maximum: 1_000 }
+    validates :price, numericality: { only_integer: true }
     validates :image
   end
 end

23f05e24eff8e32d464d1ad342a955ea57fa9dad
mokomoko-ogata <nobodyknows2405@gmail.com>
2021-05-01 11:41:51 +0900

    コントローラー修正

--- app/controllers/buys_list_controller.rb
+++ app/controllers/buys_list_controller.rb
@@ -1,6 +1,6 @@
 class BuysListController < ApplicationController
   def index
-    
+    @buy_list = BuyList.all
   end
 
   def new
@@ -10,7 +10,7 @@ class BuysListController < ApplicationController
   def create
     @buy_list = BuyList.new(buy_list_params)
     if @buy_list.save
-      redirect_to buy_lists_path(current_user.id)
+      redirect_to buys_list_index_path(current_user.id)
     else
       render :new
     end
@@ -19,6 +19,6 @@ class BuysListController < ApplicationController
   private
 
   def buy_list_params
-    params.require(:buy_list).permit(:item_name, :memo, :amount, :unit_id).merge(user_id: current_user.id)
+    params.require(:buy_list).permit(:item_name, :buy_memo, :amount, :unit_id).merge(user_id: current_user.id)
   end
 end

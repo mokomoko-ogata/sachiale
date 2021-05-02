a077de7f8710a672b37f30cfa69404019a0968d6
mokomoko-ogata <nobodyknows2405@gmail.com>
2021-05-02 12:34:16 +0900

    バリデーション修正

--- app/models/buy_list.rb
+++ app/models/buy_list.rb
@@ -3,5 +3,5 @@ class BuyList < ApplicationRecord
 
   validates :item_name, presence: true, length: { maximum: 40 }
   validates :buy_memo, length: { maximum: 1_000 }
-  validates :amount, numericality: { with: /\A[0-9]+\z/ }, numericality: { only_integer: true }
+  validates :amount, numericality: { only_integer: true }, numericality: { with: /\s |\A[0-9]+\z/ }, allow_nil: true
 end

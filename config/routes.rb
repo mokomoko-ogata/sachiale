3b03ad65402d5e21fba010c20a606f8a6a8d2d44
mokomoko-ogata <nobodyknows2405@gmail.com>
2021-05-02 18:36:20 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root to: "blogs#index"
   resources :blogs
   resources :items
-  resources :buys_list, only: [:index, :new, :create]
+  resources :buys_list, only: [:index, :new, :create, :edit, :update]
 end

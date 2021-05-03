65a2e38c881e21465a3d434a0142b9e66e60891b
mokomoko-ogata <nobodyknows2405@gmail.com>
2021-05-03 17:49:03 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "blogs#index"
-  resources :blogs
+  resources :blogs do
+    resources :comments, only: [:new, :create]
+  end
   resources :items
   resources :buys_list, only: [:index, :new, :create, :edit, :update, :destroy]
 end

d560475d32946f2e201d66bad5e3df23c003bef5
mokomoko-ogata <nobodyknows2405@gmail.com>
2021-05-03 17:51:34 +0900

    コントローラーにアクション定義

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,17 @@
+class CommentsController < ApplicationController
+
+  def new
+    @comments = Comment.all
+    @comment = Comment.new 
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, blog_id: params[:blog_id])
+  end
+end

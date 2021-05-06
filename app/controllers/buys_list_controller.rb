66671fe2c9f61f7e17de59caf54d7363b795c2fe
mokomoko-ogata <nobodyknows2405@gmail.com>
2021-05-06 16:23:25 +0900

    リファクタリングとリダイレクト設定

--- app/controllers/buys_list_controller.rb
+++ app/controllers/buys_list_controller.rb
@@ -1,5 +1,8 @@
 class BuysListController < ApplicationController
   before_action :authenticate_user!, only: [:index, :new, :create, :edit, :update, :destroy]
+  before_action :set_buy_list, only: [:edit, :update, :destroy]
+  before_action :move_to_top, only: [:edit, :update, :destroy]
+
 
   def index
     @buy_list = BuyList.where(user_id: current_user.id)
@@ -19,11 +22,9 @@ class BuysListController < ApplicationController
   end
 
   def edit
-    @buy_list = BuyList.find(params[:id])
   end
 
   def update
-    @buy_list = BuyList.find(params[:id])
     if @buy_list.update(buy_list_params)
       redirect_to buys_list_index_path(current_user.id)
     else
@@ -32,7 +33,6 @@ class BuysListController < ApplicationController
   end
 
   def destroy
-    @buy_list = BuyList.find(params[:id])
     @buy_list.destroy
     redirect_to buys_list_index_path(current_user.id)
   end
@@ -41,5 +41,13 @@ class BuysListController < ApplicationController
 
   def buy_list_params
     params.require(:buy_list).permit(:item_name, :buy_memo, :amount, :unit_id).merge(user_id: current_user.id)
+  end
+
+  def set_buy_list
+    @buy_list = BuyList.find(params[:id])
+  end
+
+  def move_to_top
+    redirect_to blogs_path unless current_user.id == @buy_list.user_id
   end
 end

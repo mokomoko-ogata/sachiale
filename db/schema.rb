575136b06b5ba36478cb85766e8c764c61af8860
mokomoko-ogata <nobodyknows2405@gmail.com>
2021-05-13 19:33:14 +0900

    マイグレーション実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_02_103053) do
+ActiveRecord::Schema.define(version: 2021_05_13_100952) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 2021_05_02_103053) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "blog_items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "blog_id"
+    t.bigint "item_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["blog_id"], name: "index_blog_items_on_blog_id"
+    t.index ["item_id"], name: "index_blog_items_on_item_id"
   end
 
   create_table "blogs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -103,6 +112,8 @@ ActiveRecord::Schema.define(version: 2021_05_02_103053) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "blog_items", "blogs"
+  add_foreign_key "blog_items", "items"
   add_foreign_key "blogs", "users"
   add_foreign_key "buy_lists", "users"
   add_foreign_key "items", "users"

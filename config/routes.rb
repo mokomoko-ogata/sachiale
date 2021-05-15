b0d71daf1cc80e9303f8f6b239a376d02d049b4e
mokomoko-ogata <nobodyknows2405@gmail.com>
2021-05-15 23:31:39 +0900

    searchのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "blogs#index"
   get 'items/search'
+  get 'blogs/search'
   resources :blogs do
     resources :comments, only: [:new, :create]
   end

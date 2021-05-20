c150b426318aa7bf5eef83868a9fc4ad413a5ec6
mokomoko-ogata <nobodyknows2405@gmail.com>
2021-05-20 23:38:26 +0900

    idの追加

--- app/javascript/category.js
+++ app/javascript/category.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function(){
-  if ( document.getElementById('item-category')){
+  if ( document.getElementById('category-form')){
     document.getElementById('item-category').addEventListener('change', function(){
       const category = document.getElementsByName('item[category_id]').value;
       console.log(category)

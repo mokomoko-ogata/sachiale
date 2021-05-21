1d5fa0ca789c5f2ad18a52dd212a6e79b2fca1f0
mokomoko-ogata <nobodyknows2405@gmail.com>
2021-05-21 23:18:22 +0900

    カテゴリー選択の要素取得

--- app/javascript/category.js
+++ app/javascript/category.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(){
   if ( document.getElementById('category-form')){
-    document.getElementById('item-category').addEventListener('change', function(){
-      const category = document.getElementsByName('item[category_id]').value;
+      document.getElementById('item-category').addEventListener('change', function(){
+      const category = document.getElementById('item-category').value;
       console.log(category)
     });
   }

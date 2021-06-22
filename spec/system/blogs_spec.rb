b575802a1f5da105c95b963042877f598dba94fa
mokomoko-ogata <nobodyknows2405@gmail.com>
2021-06-22 23:15:28 +0900

    レシピ投稿機能のexampleの洗い出し

--- spec/system/blogs_spec.rb
+++ spec/system/blogs_spec.rb
@@ -1,9 +1,28 @@
 require 'rails_helper'
 
-RSpec.describe "Blogs", type: :system do
+RSpec.describe "レシピ投稿", type: :system do
   before do
-    driven_by(:rack_test)
+    @user = FactoryBot.create(:user)
+    @blog = FactoryBot.build(:blog)
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  context 'レシピ投稿できるとき' do
+    it 'ログインしたユーザーはレシピ投稿できる' do
+      # ログインする
+      # レシピ投稿ページへのリンクがある
+      # レシピ投稿ページに遷移する
+      # フォームに入力する
+      # 投稿するとBlogモデルのカウントが1上がる
+      # トップページに遷移することを確認する
+      # トップページに先ほど投稿した内容のレシピが存在していることを確認する(画像)
+      # トップページに先ほど投稿した内容のレシピが存在していることを確認する(レシピ名)
+    end
+  end
+
+  context 'レシピ投稿できないとき' do
+    it 'ログインしていないとレシピ投稿ページに遷移できない' do
+      # トップページに遷移する
+      # レシピ投稿ページに遷移しようとするとログインページにリダイレクトされる
+    end
+  end
 end

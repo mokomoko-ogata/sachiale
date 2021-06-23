23f4c14256520fc896a3f19cf671f80a0aa23df7
mokomoko-ogata <nobodyknows2405@gmail.com>
2021-06-23 23:44:09 +0900

    レシピ投稿機能統合テスト

--- spec/system/blogs_spec.rb
+++ spec/system/blogs_spec.rb
@@ -9,20 +9,40 @@ RSpec.describe "レシピ投稿", type: :system do
   context 'レシピ投稿できるとき' do
     it 'ログインしたユーザーはレシピ投稿できる' do
       # ログインする
+      visit new_user_session_path
+      fill_in 'user[email]', with: @user.email
+      fill_in 'user[password]', with: @user.password
+      find('input[name="commit"]').click
+      expect(current_path).to eq(root_path)
       # レシピ投稿ページへのリンクがある
+      expect(page).to have_content('投稿する')
       # レシピ投稿ページに遷移する
+      visit new_blog_path
       # フォームに入力する
+      attach_file 'item-image', "#{Rails.root}/public/images/test-image.png"
+      fill_in 'blog[recipe_name]', with: @blog.recipe_name
+      fill_in 'blog[explain]', with: @blog.explain
+      fill_in 'blog[price]', with: @blog.price
       # 投稿するとBlogモデルのカウントが1上がる
+      expect do
+        find('input[name="commit"]').click
+      end.to change { Blog.count }.by(1)
       # トップページに遷移することを確認する
+      expect(current_path).to eq(root_path)
       # トップページに先ほど投稿した内容のレシピが存在していることを確認する(画像)
+      expect(page).to have_selector("img[src$='test-image.png']")
       # トップページに先ほど投稿した内容のレシピが存在していることを確認する(レシピ名)
+      expect(page).to have_content(@blog.recipe_name)
     end
   end
 
   context 'レシピ投稿できないとき' do
     it 'ログインしていないとレシピ投稿ページに遷移できない' do
       # トップページに遷移する
+      visit root_path
       # レシピ投稿ページに遷移しようとするとログインページにリダイレクトされる
+      get new_blog_path
+      expect(response).to redirect_to '/users/sign_in'
     end
   end
 end

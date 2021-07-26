acde820be2086e451a7a9dd0070156af0958b0ec
mokomoko-ogata <nobodyknows2405@gmail.com>
2021-07-26 18:44:38 +0900

    コメント投稿機能統合テストexampleの洗い出し

--- spec/system/comments_spec.rb
+++ spec/system/comments_spec.rb
@@ -1,9 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe "Comments", type: :system do
+RSpec.describe "コメント投稿", type: :system do
   before do
-    driven_by(:rack_test)
+    @blog = FactoryBot.create(:blog)
+    @user = FactoryBot.create(:user)
+    @comment = Faker::Lorem.sentence
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  context 'コメント投稿ができるとき' do
+    it 'ログインしたユーザーはレシピ詳細ページでコメント投稿ができる' do
+      # ログインする
+      # レシピ詳細ページに遷移する
+      # フォームに入力する
+      # コメントを投稿するとCommentモデルのカウントが1上がる事を確認する
+      # 詳細ページに先程のコメント内容が表示されている事を確認する
+    end
+  end
+
+  context 'コメント投稿ができないとき' do
+    it 'ログインしていないユーザーはレシピ詳細ページでコメント投稿できない' do
+      # レシピ詳細ページに遷移する
+      # フォームに情報を入力する
+      # コメントを送信してもCommentモデルのカウントは変わらない
+    end
+  end
 end
